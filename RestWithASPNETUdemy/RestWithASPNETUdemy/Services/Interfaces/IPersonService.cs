da76efe10ac3213deed637c22a1ed574d943ffff
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2025-09-11 14:42:53 -0300

    Creacao da interface Person e assinatura dos metodos

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Interfaces/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Interfaces
+{
+    public interface IPersonService
+    {
+        List<Person> FindAll();
+        Person FindById(long id);
+        Person Create(Person person);
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
