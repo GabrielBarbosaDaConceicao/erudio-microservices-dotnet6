e133302ee93554c245e30e01739412e47c8b986f
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2025-09-11 18:13:36 -0300

    Criacao dos endpoints para PersonController

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -1,15 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services.Interfaces;
 
 namespace RestWithASPNETUdemy.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/v1/[controller]")]
     public class PersonController : ControllerBase
     {
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Sum(string firstNumber, string secondNumber)
+        private readonly IPersonService _personService;
+        public PersonController(IPersonService personService)
         {
-            return BadRequest("Invalid Input");
+            _personService = personService;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> Get()
+        {
+            var persons = _personService.FindAll();
+            return Ok(persons);
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<Person> Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var result = _personService.Create(person);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _personService.Update(person);
+            return Ok(person);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+
     }
 }
--- RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -1,8 +1,13 @@
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IPersonService, PersonService>();
 
 var app = builder.Build();
 
